7dc0065809ee799796c147fc289e369d3dd3ed6b
Satwik Kumar Pradhan <satwik2954kumar@gmail.com>
2026-06-08 00:48:55 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello Satwik. I am Docker');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
